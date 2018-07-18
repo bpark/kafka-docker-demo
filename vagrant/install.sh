4616217c678165b61975a3ae3dc3793fef2ccd40
bpark <burt.parkers@gmail.com>
2018-07-18 23:08:44 +0200

    updated docker compose version

--- vagrant/install.sh
+++ vagrant/install.sh
@@ -12,7 +12,7 @@ sudo systemctl enable docker-latest
 sudo systemctl start docker-latest
 
 echo "installing docker-compose"
-sudo curl -L https://github.com/docker/compose/releases/download/1.13.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
 
